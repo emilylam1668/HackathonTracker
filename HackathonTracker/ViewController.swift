4c8ae701b0839b2980140e7960acf6f104ee2c98
Emily Lam <emilylam1668@users.noreply.github.com>
2018-07-18 15:03:29 -0700

    custom cell working

--- HackathonTracker/ViewController.swift
+++ HackathonTracker/ViewController.swift
@@ -15,17 +15,35 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     override func viewDidLoad() {
         super.viewDidLoad()
+//        tableView.delegate = self
+//        tableView.dataSource = self
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 10
     }
     
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "tableViewCell", for: indexPath)
+//        cell.textLabel?.text = "Cell Row: \(indexPath.row) Section: \(indexPath.section)"
+//        return cell
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "tableViewCell", for: indexPath)
-        cell.textLabel?.text = "Cell Row: \(indexPath.row) Section: \(indexPath.section)"
+        let cell = tableView.dequeueReusableCell(withIdentifier: "displayDataCell") as! displayDataCell
+        
+        switch indexPath.row {
+        case 0:
+            cell.amountLabel.text = "testing"
+            
+        default:
+            cell.amountLabel.text = ""
+            cell.dateCreatedLabel.text = ""
+            cell.thicknessLabel.text = ""
+        }
         return cell
     }
+    
+    @IBOutlet weak var tableView: UITableView!
 
 }
 
