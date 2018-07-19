28e23969ab913f39f09c6f5071f5d861ddfef9bb
Emily Lam <emilylam1668@users.noreply.github.com>
2018-07-19 15:03:16 -0700

    Fixed structure + coredata file edited

--- HackathonTracker/CoreDataHelper.swift
+++ HackathonTracker/CoreDataHelper.swift
@@ -7,4 +7,53 @@
 //
 
 import Foundation
+import UIKit
 import CoreData
+
+struct CoreDataHelper {
+    static let context: NSManagedObjectContext = {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            fatalError()
+        }
+        
+        let persistentContainer = appDelegate.persistentContainer
+        let context = persistentContainer.viewContext
+        
+        return context
+    }()
+    
+    
+    static func newData() -> PData {
+        let pData = NSEntityDescription.insertNewObject(forEntityName: "PData", into: context) as! PData
+        
+        return pData
+    }
+    
+    static func saveData() {
+        do {
+            try context.save()
+        } catch let error {
+            print("Could not save \(error.localizedDescription)")
+        }
+    }
+    
+    static func delete(pData: PData) {
+        context.delete(pData)
+        
+        saveData()
+    }
+    
+    
+    static func retrieveData() -> [PData] {
+        do {
+            let fetchRequest = NSFetchRequest<PData>(entityName: "PData")
+            let results = try context.fetch(fetchRequest)
+            
+            return results
+        } catch let error {
+            print("Could not fetch \(error.localizedDescription)")
+            
+            return []
+        }
+    }
+}
